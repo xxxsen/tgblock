547f676dab3eede9837a3a6db292cf4170b98c28
xxxtest <xxxtest@xx.co>
2021-12-19 21:51:41 +0800

    feat: reverse only

--- shortten/shorttern.go
+++ shortten/shorttern.go
@@ -3,9 +3,19 @@ package shortten
 import "context"
 
 func Encode(ctx context.Context, plain string) (string, error) {
-	return plain, nil
+	//TODO:
+	return reverseString(plain), nil
 }
 
 func Decode(ctx context.Context, enc string) (string, error) {
-	return enc, nil
+	//TODO:
+	return reverseString(enc), nil
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
 }
