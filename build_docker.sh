ea79ae44fcd22a01539ef10a2e1fb9019287afca
xxxtest <xxxtest@xx.co>
2022-01-01 14:43:13 +0800

    chore: update version

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=v0.0.1
+VERSION=v0.0.2
 
-docker buildx build -t xxxsen/tgblock:${VERSION} --platform=linux/amd64,linux/arm64 . --push
+docker buildx build -t xxxsen/tgblock:${VERSION} -t xxxsen/tgblock:latest --platform=linux/amd64,linux/arm64 . --push
